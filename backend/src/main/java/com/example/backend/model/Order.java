afe7d4520db65a04d9a46e6c870f094ccd7bc735
Weverson Lima <weversonbdelima@gmail.com>
2024-09-01 15:18:03 -0300

    Feat: created order model

---
+++ backend/src/main/java/com/example/backend/model/Order.java
@@ -0,0 +1,61 @@
+package com.example.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer amount;
+    private String status; 
+
+    @OneToOne(cascade = CascadeType.ALL) 
+    @JoinColumn(name = "credit_card_id")
+    private CreditCard creditCard;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getAmount() {
+        return amount != null ? amount : 0; 
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
+}
