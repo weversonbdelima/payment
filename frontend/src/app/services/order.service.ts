d59c806f5ca62b6c675507b438a580486cf58dfa
Weverson Lima <weversonbdelima@gmail.com>
2024-09-01 15:40:56 -0300

    Feat: created order service

---
+++ frontend/src/app/services/order.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CustomHttpClient } from './custom-http-client.service';
+
+import { environment } from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  private apiUrl = environment.apiUrl + '/orders'; // Ajuste a URL conforme necessário
+
+  constructor(private customHttpClient: CustomHttpClient) { }
+
+  async registerOrder(order: any) {
+    return await this.customHttpClient.post(this.apiUrl, order,);
+  }
+
+  async getOrders() {
+    return await this.customHttpClient.get(this.apiUrl);
+  }
+}
