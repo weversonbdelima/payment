d5e9c2c1a84a18a6f230888ffb75184abccbf6ce
Weverson Lima <weversonbdelima@gmail.com>
2024-09-01 12:32:59 -0300

    Feat: created order controller

---
+++ backend/src/main/java/com/example/backend/controller/OrderController.java
@@ -0,0 +1,124 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.CreditCard;
+
+import com.example.backend.model.Order;
+import com.example.backend.model.Payment;
+import com.example.backend.model.PaymentRequest;
+import com.example.backend.service.CieloService;
+import com.example.backend.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CieloService.class);
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private CieloService cieloService;
+
+    @PostMapping
+    public ResponseEntity<Object> createOrder(@RequestBody Order order) {
+
+        try {
+            order.setStatus("payment-pendent");
+
+            order = orderService.createOrder(order);
+
+            CreditCard creditCard = new CreditCard();
+            creditCard.setCardNumber(order.getCreditCard().getCardNumber());
+            creditCard.setHolder(order.getCreditCard().getHolder());
+            creditCard.setExpirationDate(order.getCreditCard().getExpirationDate());
+            creditCard.setSecurityCode(order.getCreditCard().getSecurityCode());
+            creditCard.setSaveCard(order.getCreditCard().isSaveCard());
+            creditCard.setBrand(order.getCreditCard().getBrand());
+
+            Payment payment = new Payment();
+            payment.setType("CreditCard");
+            payment.setCreditCard(creditCard);
+            payment.setCurrency("BRL");
+            payment.setCountry("BRA");
+            payment.setInstallments(1);
+            payment.setInterest("ByMerchant");
+            payment.setCapture(true);
+            payment.setAmount(order.getAmount());
+
+            PaymentRequest paymentRequest = new PaymentRequest();
+            paymentRequest.setPayment(payment);
+            paymentRequest.setMerchantOrderId(order.getId().toString());
+
+            boolean isAprovvedPayment = cieloService.processPayment(paymentRequest);
+
+            if (isAprovvedPayment) {
+                order.setStatus("payment-approved");
+            } else {
+                order.setStatus("payment-not-approved");
+            }
+
+            return ResponseEntity.ok(order);
+        } catch (Exception e) {
+            logger.error("Failed to create order: ", e);
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("message", "Erro interno, tente novamente.");
+            return ResponseEntity.status(500).body(errorResponse);
+        }
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Object> cancelOrder(@PathVariable Long id) {
+        try {
+            logger.info("Cancel order with ID: {}", id);
+
+            Order order = orderService.getOrder(id);
+
+            if ("canceled".equals(order.getStatus())) {
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("message", "Erro ao tentar cancelar o pedido. O pedido já está cancelado.");
+                return ResponseEntity.badRequest().body(errorResponse);
+            }
+
+            Order orderCanceled = orderService.cancelOrder(order);
+
+            boolean isCanceled = cieloService.cancel(orderCanceled);
+
+            if (isCanceled) {
+                return ResponseEntity.ok(orderCanceled);
+            } else {
+
+                Map<String, String> errorResponse = new HashMap<>();
+                errorResponse.put("message", "Erro ao tentar cancelar pedido.");
+                return ResponseEntity.badRequest().body(errorResponse);
+            }
+
+        } catch (Exception e) {
+            logger.error("Failed to cancel Order with ID: {}: ", id, e);
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("message", "Erro interno, tente novamente.");
+            return ResponseEntity.status(500).body(errorResponse);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllOrders() {
+        try {
+            logger.info("Get all orders");
+            return ResponseEntity.ok(orderService.getAllOrders());
+        } catch (Exception e) {
+            logger.error("Failed to get all orders: ", e);
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("message", "Erro interno, tente novamente.");
+            return ResponseEntity.status(500).body(errorResponse);
+        }
+    }
+}
