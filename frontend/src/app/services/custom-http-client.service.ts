e5cd95752c459ea867b9e89ed6bfaf5083ccb1ab
Weverson Lima <weversonbdelima@gmail.com>
2024-09-01 15:43:38 -0300

    Feat: created client custom

---
+++ frontend/src/app/services/custom-http-client.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CustomHttpClient {
+
+    private headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + environment.token
+    };
+
+    constructor() { }
+
+    private handleError(error: AxiosError) {
+
+        return Promise.reject(error.message || 'Erro desconhecido');
+    }
+
+    async get(url: string) {
+        const config: AxiosRequestConfig = {
+            headers: this.headers,
+        };
+
+        try {
+            const response = await axios.get(url, config);
+            return response.data;
+        } catch (error) {
+            return this.handleError(error as AxiosError);
+        }
+    }
+
+    async post(url: string, body: any) {
+        const config: AxiosRequestConfig = {
+            headers: this.headers,
+        };
+
+        try {
+            const response = await axios.post(url, body, config);
+            return response.data;
+        } catch (error) {
+            return this.handleError(error as AxiosError);
+        }
+    }
+}
