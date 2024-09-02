6b47c59204d9f37f4528baf02286be5e36fb9662
Weverson Lima <weversonbdelima@gmail.com>
2024-09-02 17:19:29 -0300

    Feat: added new logs

--- backend/src/main/java/com/example/backend/utils/CryptoUtils.java
+++ backend/src/main/java/com/example/backend/utils/CryptoUtils.java
@@ -11,43 +11,53 @@ import java.util.Base64;
 
 public class CryptoUtils {
 
-    private static final String ALGORITHM = "AES/ECB/PKCS5Padding"; // Modificado para incluir padding e modo ECB
+    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
     private static final Logger logger = LoggerFactory.getLogger(CryptoUtils.class);
 
     static public String decrypt(String encryptedData, String secretKey) throws Exception {
-        logger.info("Iniciando o processo de descriptografia...");
+        logger.info("Starting the decryption process...");
 
-        // Log da chave secreta (não recomendado em produção)
-        logger.info("Chave secreta: {}", secretKey);
-
-        // Verifique se a chave tem o tamanho correto
         if (secretKey.length() != 16) {
-            throw new IllegalArgumentException("A chave deve ter 16 bytes (128 bits).");
+            logger.error("The key must be 16 bytes (128 bits).");
+            throw new IllegalArgumentException("The key must be 16 bytes (128 bits).");
         }
 
         SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES");
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, keySpec);
 
-        logger.info("Descriptografando os dados...");
+        logger.info("Decrypting data...");
         byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
 
         String decryptedData = new String(decryptedBytes, "UTF-8");
-        logger.info("Descriptografia concluída com sucesso.");
+        logger.info("Decryption completed successfully.");
 
         return decryptedData;
     }
 
-    // Método de criptografia para teste
     static public String encrypt(String data, String secretKey) throws Exception {
+        logger.info("Starting the encryption process...");
+
+        if (secretKey.length() != 16) {
+            logger.error("The key must be 16 bytes (128 bits).");
+            throw new IllegalArgumentException("The key must be 16 bytes (128 bits).");
+        }
+
         SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES");
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+
+        logger.info("Encrypting data...");
         byte[] encryptedBytes = cipher.doFinal(data.getBytes("UTF-8"));
-        return Base64.getEncoder().encodeToString(encryptedBytes);
+        String encryptedData = Base64.getEncoder().encodeToString(encryptedBytes);
+
+        logger.info("Encryption completed successfully.");
+
+        return encryptedData;
     }
 
     static public CreditCard decryptCard(CreditCard card, String secretKey) throws Exception {
+        logger.info("Starting the decryption of credit card...");
 
         CreditCard decryCard = new CreditCard();
 
@@ -57,7 +67,8 @@ public class CryptoUtils {
         decryCard.setSecurityCode(decrypt(card.getSecurityCode(), secretKey));
         decryCard.setBrand(decrypt(card.getBrand(), secretKey));
 
-        return decryCard;
+        logger.info("Credit card decryption completed successfully.");
 
+        return decryCard;
     }
 }
