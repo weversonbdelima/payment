c38e0f9473016b56b810cb7bc05995396c3ec28c
Weverson Lima <weversonbdelima@gmail.com>
2024-09-02 18:06:45 -0300

    Refactor: removed comments

--- frontend/src/app/order-list/order-list.component.ts
+++ frontend/src/app/order-list/order-list.component.ts
@@ -32,21 +32,21 @@ export class OrderListComponent implements OnInit {
     try {
       const order = await this.orderService.cancelOrder(id);
 
-      // Check if the order was successfully canceled
+
       if (order && order.status === "canceled") {
         alert('Pedido cancelado!')
 
         await this.fetchOrders();
         console.log("Order canceled successfully:", order);
-        // Additional actions can be performed here, such as notifying the user or updating the UI
+   
       } else {
         console.log("Failed to cancel order. Current status:", order ? order.status : "No order found");
-        // Handle case where the order was not canceled
+     
       }
     } catch (error: any) {
 
       const errorMessage = error?.message || "Erro desconhecido. Tente novamente.";
-      alert(errorMessage); // Show the error message in an alert
+      alert(errorMessage); 
       console.error("Error while canceling the order:", error);
     }
   }
@@ -57,19 +57,19 @@ export class OrderListComponent implements OnInit {
     const lastFourDigits = cleaned.slice(-4);
     const maskedPart = '*'.repeat(cleaned.length - 4);
 
-    const formattedMasked = maskedPart + lastFourDigits; // Máscara com os últimos 4 dígitos
-    const formatted = formattedMasked.replace(/(.{4})/g, '$1 ').trim(); // Formatação com espaço
+    const formattedMasked = maskedPart + lastFourDigits; 
+    const formatted = formattedMasked.replace(/(.{4})/g, '$1 ').trim(); 
 
     return formatted;
   }
 
   getCardTypePath(cardNumber: string): string {
     const path = "assets/images/card_brand/";
-    const cleanedNumber = cardNumber.replace(/\D/g, ''); // Remove qualquer caractere não numérico
+    const cleanedNumber = cardNumber.replace(/\D/g, ''); 
 
-    // Verifique se o número do cartão tem comprimento válido
+
     if (cleanedNumber.length < 13 || cleanedNumber.length > 19) {
-      return path + 'credit-card.png'; // Retorna imagem padrão se o comprimento for inválido
+      return path + 'credit-card.png'; 
     }
 
     if (cleanedNumber.startsWith('4')) {
@@ -83,7 +83,7 @@ export class OrderListComponent implements OnInit {
     } else if (/^(6011|65|64[4-9]|622(12[6-9]|1[3-9]\d|[2-8]\d{2}|9[01]\d|92[0-5]))/.test(cleanedNumber)) {
       return path + 'discover-card.svg';
     } else {
-      return path + 'credit-card.png'; // Imagem padrão para cartões não identificados
+      return path + 'credit-card.png'; 
     }
   }
 }
