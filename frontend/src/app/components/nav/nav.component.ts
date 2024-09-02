df9cf19ffaf8ee0c4a1fda1e54ab39336dc1684a
Weverson Lima <weversonbdelima@gmail.com>
2024-09-02 11:09:38 -0300

    Refactor: component

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,14 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
-  public isCollapsed: boolean = true;
 
 }
