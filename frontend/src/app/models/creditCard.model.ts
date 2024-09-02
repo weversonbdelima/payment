aa830ebfa1f5fe8d740d79875634f3a40b64e7ee
Weverson Lima <weversonbdelima@gmail.com>
2024-09-02 11:07:59 -0300

    Refactor: model

--- frontend/src/app/models/creditCard.model.ts
+++ frontend/src/app/models/creditCard.model.ts
@@ -1,21 +1,7 @@
-export class CreditCard {
+export interface CreditCard {
     cardNumber: string;
     holder: string;
     expirationDate: string;
     securityCode: string;
     brand: string;
-
-    constructor(
-        cardNumber: string,
-        holder: string,
-        expirationDate: string,
-        securityCode: string,
-        brand: string
-    ) {
-        this.cardNumber = cardNumber;
-        this.holder = holder;
-        this.expirationDate = expirationDate;
-        this.securityCode = securityCode;
-        this.brand = brand;
-    }
 }
